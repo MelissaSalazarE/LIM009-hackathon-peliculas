d17e21f361db78c8429da2cc8b5364b29246157b
melilove <melissa.salazar13@yahoo.com>
2019-04-17 09:34:06 -0500

    creando el carrusel

--- data.js
+++ data.js
@@ -1,234 +1,91 @@
-const MARVEL= [
+const MARVEL = {
+  "AVENGERS":[{
+    "titulo":"The Avengers",
+    "imagen":"https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    "descripcion":"Cuando un enemigo inesperado surge como una gran amenaza para la seguridad mundial, Nick Fury, director de la Agencia SHIELD, decide reclutar a un equipo para salvar al mundo de un desastre casi seguro. Adaptación del cómic de Marvel “Los Vengadores”, el legendario grupo de superhéroes formado por, entre otros, Ironman, Hulk, Thor y Capitán América.",
+    },
     {
-    "Title": "The Avengers",
-    "Year": "2012",
-    "Rated": "PG-13",
-    "Released": "04 May 2012",
-    "Runtime": "143 min",
-    "Genre": "Action, Adventure, Sci-Fi",
-    "Director": "Joss Whedon",
-    "Writer": "Joss Whedon (screenplay), Zak Penn (story), Joss Whedon (story)",
-    "Actors": "Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth",
-    "Plot": "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
-    "Language": "English, Russian, Hindi",
-    "Country": "USA",
-    "Awards": "Nominated for 1 Oscar. Another 38 wins & 79 nominations.",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-    "Ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "8.1/10"
-      },
-      {
-        "Source": "Rotten Tomatoes",
-        "Value": "92%"
-      },
-      {
-        "Source": "Metacritic",
-        "Value": "69/100"
-      }
+    "titulo":"Avengers Age Of Ultron",
+    "imagen":"https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersageofultron_lob_crd_03.jpg",
+    "descripcion":"Cuando Tony Stark intenta iniciar un programa para garantizar la paz, las cosas se ponen feas y Los Vengadores deberán detener al villano Ultron, que hará todo lo posible por arruinar sus planes.",
+    },
+    {
+    "titulo":"Infinity War",
+    "imagen":"https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersinfinitywar_lob_crd_02_1.jpg",
+    "descripcion":"Vengadores: Infinity War seguirá la lucha de los superhéroes de Marvel contra el mayor villano al que se han enfrentado nunca: Thanos. Su único objetivo será detener a este poderoso antagonista e impedir que se haga con el control de la galaxia. De nuevo veremos al grupo formado por Iron Man, Capitán América, Viuda negra, Ant-Man, Ojo de Halcón, Thor y Hulk, entre otros. En su nueva e impactante aventura, las Gemas del Infinito estarán en juego, unos querrán protegerlas y otros controlarlas, ¿quién ganará?",
+    }
     ],
-    "Metascore": "69",
-    "imdbRating": "8.1",
-    "imdbVotes": "1,157,818",
-    "imdbID": "tt0848228",
-    "Type": "movie",
-    "DVD": "25 Sep 2012",
-    "BoxOffice": "$623,279,547",
-    "Production": "Walt Disney Pictures",
-    "Website": "http://marvel.com/avengers_movie",
-    "Response": "true"
-  },
-  {
-    "Title": "The Incredible Hulk",
-    "Year": "2008",
-    "Rated": "PG-13",
-    "Released": "13 Jun 2008",
-    "Runtime": "112 min",
-    "Genre": "Action, Adventure, Sci-Fi",
-    "Director": "Louis Leterrier",
-    "Writer": "Zak Penn (screenplay), Zak Penn (screen story), Stan Lee (Marvel comic book), Jack Kirby (Marvel comic book)",
-    "Actors": "Edward Norton, Liv Tyler, Tim Roth, William Hurt",
-    "Plot": "Bruce Banner, a scientist on the run from the U.S. Government, must find a cure for the monster he turns into, whenever he loses his temper.",
-    "Language": "English, Portuguese, Spanish",
-    "Country": "USA, Canada",
-    "Awards": "1 win & 8 nominations.",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMTUyNzk3MjA1OF5BMl5BanBnXkFtZTcwMTE1Njg2MQ@@._V1_SX300.jpg",
-    "Ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "6.7/10"
-      },
-      {
-        "Source": "Rotten Tomatoes",
-        "Value": "67%"
-      },
-      {
-        "Source": "Metacritic",
-        "Value": "61/100"
-      }
-    ],
-    "Metascore": "61",
-    "imdbRating": "6.7",
-    "imdbVotes": "392,435",
-    "imdbID": "tt0800080",
-    "Type": "movie",
-    "DVD": "21 Oct 2008",
-    "BoxOffice": "$134,518,390",
-    "Production": "Universal Pictures",
-    "Website": "http://www.incrediblehulk.com/",
-    "Response": "true"
-  },
-      {
-    "Title": "Spider-Man: Homecoming",
-    "Year": "2017",
-    "Rated": "PG-13",
-    "Released": "07 Jul 2017",
-    "Runtime": "133 min",
-    "Genre": "Action, Adventure, Sci-Fi",
-    "Director": "Jon Watts",
-    "Writer": "Jonathan Goldstein (screenplay by), John Francis Daley (screenplay by), Jon Watts (screenplay by), Christopher Ford (screenplay by), Chris McKenna (screenplay by), Erik Sommers (screenplay by), Jonathan Goldstein (screen story by), John Francis Daley (screen story by), Stan Lee (based on the Marvel comic book by), Steve Ditko (based on the Marvel comic book by), Joe Simon (Captain America created by), Jack Kirby (Captain America created by)",
-    "Actors": "Tom Holland, Michael Keaton, Robert Downey Jr., Marisa Tomei",
-    "Plot": "Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.",
-    "Language": "English, Spanish",
-    "Country": "USA",
-    "Awards": "4 wins & 9 nominations.",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNTk4ODQ1MzgzNl5BMl5BanBnXkFtZTgwMTMyMzM4MTI@._V1_SX300.jpg",
-    "Ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "7.5/10"
-      },
-      {
-        "Source": "Rotten Tomatoes",
-        "Value": "92%"
-      },
-      {
-        "Source": "Metacritic",
-        "Value": "73/100"
-      }
-    ],
-    "Metascore": "73",
-    "imdbRating": "7.5",
-    "imdbVotes": "398,278",
-    "imdbID": "tt2250912",
-    "Type": "movie",
-    "DVD": "17 Oct 2017",
-    "BoxOffice": "$334,166,825",
-    "Production": "Sony Pictures",
-    "Website": "http://www.sonypictures.com/movies/spidermanhomecoming/",
-    "Response": "true"
-  },
-        {
-    "Title": "Iron Man 3",
-    "Year": "2013",
-    "Rated": "PG-13",
-    "Released": "03 May 2013",
-    "Runtime": "130 min",
-    "Genre": "Action, Adventure, Sci-Fi",
-    "Director": "Shane Black",
-    "Writer": "Drew Pearce (screenplay by), Shane Black (screenplay by), Stan Lee (based on the Marvel comic book by), Don Heck (based on the Marvel comic book by), Larry Lieber (based on the Marvel comic book by), Jack Kirby (based on the Marvel comic book by), Warren Ellis (based on the Extremis mini-series written by), Adi Granov (based on the Extremis mini-series illustrated by)",
-    "Actors": "Robert Downey Jr., Gwyneth Paltrow, Don Cheadle, Guy Pearce",
-    "Plot": "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.",
-    "Language": "English",
-    "Country": "USA",
-    "Awards": "Nominated for 1 Oscar. Another 17 wins & 61 nominations.",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMjE5MzcyNjk1M15BMl5BanBnXkFtZTcwMjQ4MjcxOQ@@._V1_SX300.jpg",
-    "Ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "7.2/10"
-      },
-      {
-        "Source": "Rotten Tomatoes",
-        "Value": "80%"
-      },
-      {
-        "Source": "Metacritic",
-        "Value": "62/100"
-      }
-    ],
-    "Metascore": "62",
-    "imdbRating": "7.2",
-    "imdbVotes": "678,031",
-    "imdbID": "tt1300854",
-    "Type": "movie",
-    "DVD": "24 Sep 2013",
-    "BoxOffice": "$408,992,272",
-    "Production": "Walt Disney Pictures",
-    "Website": "http://IronManMovie3.com",
-    "Response": "true"
-  },
-          {
-    "Title": "The Dark World",
-    "Year": "1953",
-    "Rated": "N/A",
-    "Released": "N/A",
-    "Runtime": "60 min",
-    "Genre": "Biography, Drama",
-    "Director": "Metin Erksan",
-    "Writer": "Bedri Rahmi Eyuboglu",
-    "Actors": "Asik Veysel, Ayfer Feray, Kemal Öz, Aclan Sayilgan",
-    "Plot": "N/A",
-    "Language": "Turkish",
-    "Country": "Turkey",
-    "Awards": "N/A",
-    "Poster": "http://ia.media-imdb.com/images/M/MV5BMDA0N2Y5YTMtYmEzOC00YWZmLTlhMjEtYzVkYTkzZTJiNDBjXkEyXkFqcGdeQXVyNDk2ODQwMDc@._V1_SX300.jpg",
-    "Ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "8.1/10"
-      }
-    ],
-    "Metascore": "N/A",
-    "imdbRating": "8.1",
-    "imdbVotes": "30",
-    "imdbID": "tt0295364",
-    "Type": "movie",
-    "DVD": "N/A",
-    "BoxOffice": "N/A",
-    "Production": "N/A",
-    "Website": "N/A",
-    "Response": "true"
-  },
-            {
-    "Title": "Captain America: The Winter Soldier",
-    "Year": "2014",
-    "Rated": "PG-13",
-    "Released": "04 Apr 2014",
-    "Runtime": "136 min",
-    "Genre": "Action, Adventure, Sci-Fi, Thriller",
-    "Director": "Anthony Russo, Joe Russo",
-    "Writer": "Christopher Markus (screenplay by), Stephen McFeely (screenplay by), Joe Simon (based on the Marvel comics by), Jack Kirby (based on the Marvel comics by)",
-    "Actors": "Chris Evans, Samuel L. Jackson, Scarlett Johansson, Robert Redford",
-    "Plot": "As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat from history: an assassin known as the Winter Soldier.",
-    "Language": "English, French",
-    "Country": "USA",
-    "Awards": "Nominated for 1 Oscar. Another 5 wins & 50 nominations.",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMzA2NDkwODAwM15BMl5BanBnXkFtZTgwODk5MTgzMTE@._V1_SX300.jpg",
-    "Ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "7.8/10"
-      },
-      {
-        "Source": "Rotten Tomatoes",
-        "Value": "90%"
-      },
-      {
-        "Source": "Metacritic",
-        "Value": "70/100"
-      }
-    ],
-    "Metascore": "70",
-    "imdbRating": "7.8",
-    "imdbVotes": "646,011",
-    "imdbID": "tt1843866",
-    "Type": "movie",
-    "DVD": "09 Sep 2014",
-    "BoxOffice": "$228,636,083",
-    "Production": "Walt Disney Pictures",
-    "Website": "http://marvel.com/captainamerica",
-    "Response": "true"
-  }
-];
+  "HULK": [{
+    "Hulk1": {
+      "titulo": "Hulk: el hombre increíble",
+      "imagen": "https://pics.filmaffinity.com/the_incredible_hulk-175130941-large.jpg",
+      "descripcion": "Durante un experimento científico fallido, el doctor Bruce Banner se ve expuesto a una sobredosis de radiaciones nucleares. Aunque sale milagrosamente ileso, esta terrible experiencia le confiere el poder de transformarse en Hulk, un monstruo verde de fuerza sobrehumana e ira incontrolable",
+    },
+    "Hulk2": {
+      "titulo": "El increíble Hulk 2",
+      "imagen": "http://www.klownsasesinos.com/wp-content/uploads/2009/01/hulk2cov1.jpg",
+      "descripcion": "Bruce Banner recorre el mundo en busca de un antídoto para librarse de su alter ego. Además tendrá que hacer frente a Emil, un nuevo enemigo, lo que convertirá a Nueva York en el escenario de la última batalla entre las dos criaturas más poderosas.",
+    }
+  }],
+  "CAPITAN": [{
+    "capitan1": {
+      "titulo": "El primer Vengador",
+      "imagen": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/capita-n-ame-rica-el-primer-vengador-1525786476.jpg?crop=1xw:1xh;center,top&resize=980:*",
+      "descripcion": "Captain America: The First Avenger (Capitán América: El Primer Vengador en América Hispana y España) es una película de superhéroes basada en el superhéroe de Marvel Comics del mismo nombre. Se trata de la quinta entrega en el Universo Cinematográfico de Marvel y de la Fase Uno.",
+    },
+    "capitan2": {
+      "titulo": "El soldado de invierno",
+      "imagen": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/1/15/Captain_America_The_Winter_Soldier_Poster_3.png/revision/latest/scale-to-width-down/337?cb=20150611180401&path-prefix=es",
+      "descripcion": "Captain America: The Winter Soldier (Capitán América y el Soldado del Invierno en América Hispana y Capitán América: El Soldado de Invierno en España) es una película de superhéroes basada en el superhéroe de Marvel Comics del mismo nombre.Se trata de la secuela de Captain America: The First Avenger y The Avengers, la novena entrega del Universo Cinematográfico de Marvel y la tercera entrega de la Fase Dos. ",
+    },
+    "capitan3": {
+      "titulo": "Guerra Civil",
+      "imagen": "https://www.ecartelera.com/carteles/8100/8129/019_p.jpg",
+      "descripcion": "Capitán America: Civil War, cuya historia arranca tras los hechos ocurridos en Vengadores: La Era de Ultrón (2015), es la tercera parte de las aventuras de Steve Rogers. Los hermanos Anthony y Joe Russo, encargados de dirigir Capitán América: el Soldado de Invierno (2014), están al frente de esta nueva entrega de Marvel Studios.",
+    }
+  }],
+  "IRON": [{
+    "ironMan1": {
+      "titulo": "Iron Man 1",
+      "imagen": "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG",
+      "descripcion": "Tony Stark, playboy multimillonario, es el dueño de Stark Industries, una importante empresa de armamento. Unos terroristas le secuestran y torturan para que fabrique para ellos un misil devastador. En vez de eso decide que su empresa dejará de fabricar armas, la deja en manos de Obadiah Stane, su hombre de confianza y él se dedica a construir una potente armadura. Pero Stane le traiciona y sigue fabricando armas.",
+    },
+    "ironMan2": {
+      "titulo": "Iron Man 2",
+      "imagen": "https://images-na.ssl-images-amazon.com/images/I/91UgoL1eiUL._SY445_.jpg",
+      "descripcion": "En Iron Man 2, a Downey le acompaña un reparto cuajado de estrellas entre héroes y villanos, entre los que se encuentran Gwyneth Paltrow, Don Cheadle, Scarlett Johansson, Sam Rockwell, Mickey Rourke y Samuel L. Jackson como Nick Furia.",
+    },
+    "ironMan3": {
+      "titulo": "Iron Man 3",
+      "imagen": "https://pics.filmaffinity.com/iron_man_3-972235216-large.jpg",
+      "descripcion": "Iron Man 3 es una película de superhéroes basada en el superhéroe de Marvel Comics del mismo nombre. Se trata de la secuela de Iron Man 2 y The Avengers, la séptima entrega del Universo Cinematográfico de Marvel y la primera entrega de la Fase Dos.",
+    }
+  }],
+  "STRANGE": [{
+    "titulo": "Doctor Strange",
+    "imagen": "https://is4-ssl.mzstatic.com/image/thumb/Video118/v4/3b/59/3f/3b593fe0-6aea-c5c9-d94d-7a6cc302f6cb/pr_source.lsr/268x0w.png",
+    "descripcion": "De Marvel llega Doctor Strange: Hechicero Supremo, la historia del famoso neurocirujano Dr. Stephen Strange cuya vida cambia para siempre luego de un accidente automovilístico que lo deja sin poder mover sus manos. Cuando la medicina tradicional falla, se ve obligado a buscar esperanza y una cura en un lugar impensable.",
+  }],
+  "PANTER": [{
+    "titulo": "BLACK PANTHER",
+    "imagen": "https://repelisgo.com/_images/posters/EtApAzte7zuDm4jfqXRLfud/280x420.jpg",
+    "descripcion": "Black Panther cuenta la historia de T'Challa quien, después de los acontecimientos de Capitán América: Civil War, vuelve a casa, a la nación de Wakanda, aislada y muy avanzada tecnológicamente, para ser proclamado Rey. Pero la reaparición de un viejo enemigo pone a prueba el temple de T'Challa como Rey y Black Panther ya que se ve arrastrado a un conflicto que pone en peligro todo el destino de Wakanda y del mundo.",
+  }],
+  "THOR": [{
+    "thor1": {
+      "titulo": "thor",
+      "imagen": "https://repelisgo.com/_images/posters/EV8HivYwGLSQvoU7aGL2YXB/280x420.jpg",
+      "descripcion": "Thor es un arrogante y codicioso guerrero cuya imprudencia desata una antigua guerra. Por ese motivo, su padre Odín lo castiga desterrándolo a la Tierra para que viva entre los hombres y descubra así el verdadero sentido de la humildad. Cuando el villano más peligroso de su mundo envía a la Tierra a las fuerzas más oscuras de Asgard, Thor se dará cuenta de lo que realmente hace falta para ser un verdadero héroe.",
+    },
+    "thor2": {
+      "titulo": "El mundo oscuro",
+      "imagen": "https://repelisgo.com/_images/posters/931M1JuCzrWsbDDQTzv44nd/280x420.jpg",
+      "descripcion": "Thor lucha por restablecer el orden en el cosmos, pero una antigua raza liderada por el vengativo Malekith regresa con el propósito de volver a sumir el universo en la oscuridad. Se trata de un villano con el que ni siquiera Odín y Asgard se atreven a enfrentarse; por esa razón, Thor tendrá que emprender un viaje muy peligroso, durante el cual se reunirá con Jane Foster y la obligará a sacrificarlo todo para salvar el mundo.",
+    },
+    "thor3": {
+      "titulo": "El mundo oscuro",
+      "imagen": "https://d3fa68hw0m2vcc.cloudfront.net/59d/197155926.jpeg",
+      "descripcion": "Thor lucha por restablecer el orden en el cosmos, pero una antigua raza liderada por el vengativo Malekith regresa con el propósito de volver a sumir el universo en la oscuridad. Se trata de un villano con el que ni siquiera Odín y Asgard se atreven a enfrentarse; por esa razón, Thor tendrá que emprender un viaje muy peligroso, durante el cual se reunirá con Jane Foster y la obligará a sacrificarlo todo para salvar el mundo.",
+    }
+  }]
+} 
--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-const mostrar = document.getElementById("player");
+const box = document.getElementById('player-aven');
+const box2 = document.getElementById('player-aven2');
 
+const reproducir = (arr,box) => {
+   arr.forEach(elem => {
+      box.innerHTML += `<div class="cuadros"><img class="imgs" src=${elem["imagen"]}></div>`
+   });
+};
 
+const select = document.getElementById("aven1");
+select.addEventListener("click", () => {
+   const properity = MARVEL["AVENGERS"];
+   let arr = [];
+   arr.push(properity[0]);
+   box.style.display="block";
+   reproducir(arr,box);
+});
 
-const reproducir = (data) => {
-    const arr=data;
-    const box= document.getElementById('player');
-    arr.forEach(element => {
-       box.innerHTML=`<div class="imgs">${element["Poster"]}</div>` 
-    });
-    }
-    mostrar.innerHTML = reproducir(MARVEL);
+const select2 = document.getElementById("CapiAmerica");
+select2.addEventListener("click", () => {
+   box2.style.display="block";
+   reproducir(MARVEL["CAPITAN"],box2);
+});
